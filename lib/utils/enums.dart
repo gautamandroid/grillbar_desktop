d1b35ecf502b2ef2ce50c9cf1f87f7a533f71f9a
gautam <109837678+gautamandroid@users.noreply.github.com>
2026-01-04 18:34:02 +0530

    Flutter web deploy

--- lib/utils/enums.dart
+++
@@ -1,1 +0,0 @@
-enum ServingUnit { plate, serving, stack, basket, cup, slice, glass }
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\SDK_Flutter\flutter_3.32.1"
+export "FLUTTER_APPLICATION_PATH=E:\GitHubPortFoliyo\grill_bar_app_dekstop\grillbar_desktop"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
